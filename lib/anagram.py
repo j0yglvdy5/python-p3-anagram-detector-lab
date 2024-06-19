b6dc8a3df13640f7ecb1efc88290c26ad2562434
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-06-19 12:02:36 +0300

    final

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        matches = []
+        sorted_word = sorted(self.word)
+        
+        for candidate in candidates:
+            if len(candidate) == len(self.word) and candidate.lower() != self.word:
+                if sorted(candidate.lower()) == sorted_word:
+                    matches.append(candidate)
+
+        return matches
